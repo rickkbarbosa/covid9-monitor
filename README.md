2dfe1a2e6dd0c224c33fc0838bdb583ef5b8f9cb
Ricardo Barbosa <ricardo.barbosa@nubeliu.com>
2020-03-11 01:25:36 -0300

    Uploading content

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # covid9-monitor
 Gets information involving casualities and presents to JSON and Zabbix
+
+![Grafana](grafana_dashboard.png)
+
+
+### Usage
+
+```
+git@github.com:rickkbarbosa/covid9-monitor.git
+pip install -r requirements.txt
+```
+
+### To Zabbix
+
+Clone repository in a plase acessible by Zabbix (usually, /usr/lib/zabbix/externalscripts)
+install python requirements on requirements.txt
+Import template to zabbix dashboard (zabbix_template.xml)
+Add template to a host
+
+![Using on Zabbix](zabbix_template.png)
+
+### To grafana
+Import dashboard to grafana
+Points your Zabbix datasource
---
+++ covid9_status.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#===============================================================================
+# IDENTIFICATION DIVISION
+#        ID SVN:   $Id$
+#          FILE:  covid9_status.py
+#         USAGE:  $0
+#   DESCRIPTION:  Get Worldometers Coronavirus / Covid9 data and print to JSON
+#  REQUIREMENTS:  pandas (pip install pandas)
+#                 lxml (pip install lxml)
+#          BUGS:  ---
+#         NOTES:  ---
+#          TODO:  ---
+#        AUTHOR:  Ricardo Barbosa, rickkbarbosa@live.com
+#       COMPANY:  ---
+#       VERSION:  1.0
+#       CREATED:  2020-Mar-10 21:47 BRT
+#      REVISION:  ---
+#===============================================================================
+
+import pandas as pd
+
+url = 'https://www.worldometers.info/coronavirus/#countries'
+
+df = pd.read_html(url, index_col=0)[0]
+df = df.fillna('0')
+df = df.to_json(orient='index')
+
+print(df)
