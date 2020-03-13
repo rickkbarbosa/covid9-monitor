dcbdab0b1f074d8877f5ec6fd922fa77c81a95b9
everaldoscabral <53796085+everaldoscabral@users.noreply.github.com>
2020-03-13 09:35:28 -0300

    Revert "Update covid19_affectedcountries.py"

--- covid19_affectedcountries.py
+++ covid19_affectedcountries.py
@@ -19,13 +19,10 @@
 
 import pandas as pd
 import json
-import requests
 
 url = 'https://www.worldometers.info/coronavirus/#countries'
-header = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36","X-Requested-With": "XMLHttpRequest"}
 
-r = requests.get(url, headers=header)
-df = pd.read_html(r.text)[0]
+df = pd.read_html(url)[0]
 df = df['Country,Other']
 
 country_list = list()
